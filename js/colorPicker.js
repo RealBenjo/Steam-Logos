a0b7d12e69b5abe8fdb18b441ab20a42abe554c1
Benjamin Ipavec <benjamin.ipavec@gmail.com>
2025-12-12 20:48:11 +0100

    logo resize bar implemented

--- js/colorPicker.js
+++ js/colorPicker.js
@@ -2,12 +2,13 @@ const colPickLogoTop = document.getElementById("logoColorTop");
 const colPickLogoBottom = document.getElementById("logoColorBottom");
 const colPickLogo1 = document.getElementById("logoColor1");
 const colPickLogo0 = document.getElementById("logoColor0");
-const colorOutput = document.getElementById("color-output");
+const sizePickLogo = document.getElementById("logoSize");
 
 addEventListener("input", (event) => {
   logoTopColor = colPickLogoTop.value;
   logoBottomColor = colPickLogoBottom.value;
   logoMain1 = colPickLogo1.value;
   logoMain0 = colPickLogo0.value;
+  size = sizePickLogo.value;
   drawLogo();
 });
